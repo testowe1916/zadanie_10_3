5849e3b3a3cb53446fc7299583e058e0c57f26d3
Mariusz Sioch <mariusz.sioch@o2.pl>
2017-12-18 22:23:27 +0100

    Add js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function() {
+	var carouselList = $("#carousel ul");
+	function changeSlides() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	}
+	setInterval(changeSlide, 3000);
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem)
+		carouselList.css({marginLeft:0});
+	}
+});
